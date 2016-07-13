680a754b1fc6f02c453e371e0e1ab48c17abaf53
sergiufrent <sergiu.frent@evozone.com>
2016-07-13 17:29:09 +0300

    modifi test content page

--- src/test/java/com/tests/Test_ContentPage.java
+++ src/test/java/com/tests/Test_ContentPage.java
@@ -1,6 +1,34 @@
 package com.tests;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 
+import com.selenium.steps.HomePageSteps;
+import com.selenium.steps.LoginSteps;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import tools.Constants;
+
+@RunWith(SerenityRunner.class)
 public class Test_ContentPage {
-
+	@Managed(uniqueSession = true)
+	public WebDriver webdriver;
+	
+	@Steps
+	public LoginSteps userSteps;
+	
+	@Steps
+	public HomePageSteps home;
+	
+	@Test
+	public void createNewVacationRequest() {
+		home.is_the_home_page();
+		home.starts_searchForSignInButton();
+		userSteps.performLogin(Constants.USERNAME, Constants.PASSWORD);
+		
+	}
+	
 }
