fdb23979bad3ff393f442b557ad780bc1f9b181f
AncaCoroama <anca.delia12@yahoo.com>
2016-07-15 17:34:08 -0700

    Multiple Request Test

---
+++ src/test/java/com/tests/MyRequestCheckOneOrMoreItemsTest.java
@@ -0,0 +1,44 @@
+package com.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.selenium.steps.HeaderNavigationSteps;
+import com.selenium.steps.HomePageSteps;
+import com.selenium.steps.LeftMenuSteps;
+import com.selenium.steps.LoginSteps;
+import com.selenium.steps.MyRequestSteps;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Steps;
+import tools.Constants;
+
+@RunWith(SerenityRunner.class)
+public class MyRequestCheckOneOrMoreItemsTest extends BaseTest{
+
+	@Steps
+	public MyRequestSteps myRequestsSteps;
+	
+	@Steps
+	public LoginSteps userSteps;
+	
+	@Steps
+	public HomePageSteps home;
+	
+	@Steps
+	public HeaderNavigationSteps headerSteps;
+	
+	@Steps
+	public LeftMenuSteps leftMenuSteps;
+	
+	@Test
+	public void clickRandomElement(){
+		home.isTheHomePage();
+		home.starts_searchForSignInButton();
+		userSteps.performLogin(Constants.USERNAME, Constants.PASSWORD);
+		headerSteps.selectMenuItem();
+		leftMenuSteps.clickMyRequests();
+		myRequestsSteps.clickOneOrMoreCheckBoxItem(Constants.VACATIONTYPE_HOLIDAY,Constants.VACATIONTYPE_VACATION, Constants.VACATIONTYPE_SPECIALVACATION, Constants.VACATIONTYPE_SICKLEAVE, Constants.VACATIONTYPE_MATERNITY);
+		myRequestsSteps.clickApplyButton();
+	}
+}
