549391f81c0312a81b9832abdac1d12a289a0267
BodeaCornelia <bodea_nelly@yahoo.com>
2016-07-19 04:21:20 -0700

    added EmailVerificationPage

---
+++ src/test/java/com/tests/NVR03VerifyTheEmailTest.java
@@ -0,0 +1,56 @@
+package com.tests;
+
+import org.junit.runner.RunWith;
+
+import com.selenium.steps.HeaderNavigationSteps;
+import com.selenium.steps.HomePageSteps;
+import com.selenium.steps.LeftMenuSteps;
+import com.selenium.steps.LoginSteps;
+import com.selenium.steps.NewVacationRequestSteps;
+
+import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.annotations.UseTestDataFrom;
+import tools.Constants;
+
+@RunWith(SerenityParameterizedRunner.class)
+@UseTestDataFrom(value = Constants.CSV_FILES_PATH + "NewVacationRequestTest.csv", separator = Constants.CSV_SEPARATOR)
+public class NVR03VerifyTheEmailTest extends BaseTest {
+	
+	@Steps
+	public LoginSteps userSteps;
+
+	@Steps
+	public HomePageSteps homeSteps;
+
+	@Steps
+	public HeaderNavigationSteps headerNavigationSteps;
+
+	@Steps
+	public LeftMenuSteps leftMenuSteps;
+
+	@Steps
+	public NewVacationRequestSteps newVacationSteps;
+
+	public String year, month, day, futureYear, futureMonth, futureDay, vacationTypeName, specialVacationName;
+	
+	public void verifyTheEmail(){
+		homeSteps.isTheHomePage();
+		homeSteps.starts_searchForSignInButton();
+		userSteps.performLogin(userName, password);
+		headerNavigationSteps.selectMenuItem();
+		leftMenuSteps.clickNewVacationRequest();
+		newVacationSteps.selectStartDateField();
+		newVacationSteps.clickYear();
+		newVacationSteps.selectStartDate(year, month, day);
+		newVacationSteps.selectEndDateField();
+		newVacationSteps.clickYear();
+		newVacationSteps.selectStartDate(futureYear, futureMonth, futureDay);
+		newVacationSteps.selectVacationType(vacationTypeName);
+		newVacationSteps.selectSpecialVacation(specialVacationName);
+		newVacationSteps.clickOnSaveButton();
+		
+		newVacationSteps.waitSeconds();
+
+	}
+}
